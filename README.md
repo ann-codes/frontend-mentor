36074d811db4bf60310d6f102511f2208cf132c5
Ann <anguyen@stavvy.com>
2022-06-26 17:04:40 -0400

    small fixes

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 Just a repo to hold all/most of the FE mentor exercises using tools I prefer. 
 
-### Built with:
+### Tools:
 - [Vite](https://vitejs.dev/)
 - [React](https://reactjs.org/)
 - [TypeScript](https://www.typescriptlang.org/)
 - [React Query](https://react-query.tanstack.com/)
 - [React Router v6](https://reactrouter.com/)
-- [Jotai](https://jotai.org/) 
+- [Jotai](https://jotai.org/) || [Zustand](https://zustand.surge.sh/)
 - [Tailwind](https://tailwindcss.com/) & [HeadlessUI](https://headlessui.dev/)
 - [Styled Components](https://styled-components.com/)
+- [Vercel](https://vercel.com/)
 
 # Setup
 - clone repo, `yarn/npm install`, `yarn/npm run dev`
--- src/layouts/SneakyNav.tsx
+++ src/layouts/SneakyNav.tsx
@@ -6,36 +6,40 @@ import { ROUTE_LINKS } from '../routes/RouteList';
 
 export default function SneakyNav() {
     const mappedLinks = Object.keys(ROUTE_LINKS).map(level => (
-        <>
+        <Fragment key={level}>
             {ROUTE_LINKS[level].map(({ linkLabel, urlPath }) => (
                 <li key={urlPath}>
                     <Link to={`challenges/${level}/${urlPath}`}>{linkLabel}</Link>
                 </li>
             ))}
-        </>
+        </Fragment>
     ));
 
     return (
-        <Popover className="absolute">
-            <Popover.Button className="opacity-25 hover:opacity-100">⭐️</Popover.Button>
-            <Transition
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-0"
-                enterTo="opacity-100 translate-y-1"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-1"
-                leaveTo="opacity-0 translate-y-0"
-            >
-                <Popover.Panel className="z-10">
-                    <ul className="bg-slate-50 py-2 px-4">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        {mappedLinks}
-                    </ul>
-                </Popover.Panel>
-            </Transition>
+        <Popover className="absolute top-0 left-0">
+            {({ open }) => (
+                <>
+                    <Popover.Button className={`opacity-${open ? '100' : '25'} hover:opacity-100`}>⭐️</Popover.Button>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 translate-y-0"
+                        enterTo="opacity-100 translate-y-1"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-1"
+                        leaveTo="opacity-0 translate-y-0"
+                    >
+                        <Popover.Panel className="relative z-10 left-6">
+                            <ul className="bg-slate-50 py-2 px-4">
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                {mappedLinks}
+                            </ul>
+                        </Popover.Panel>
+                    </Transition>
+                </>
+            )}
         </Popover>
     );
 }
